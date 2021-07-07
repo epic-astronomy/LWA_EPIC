f4f677729e43dd7fbeb251bb345dbe4868fb3ab2
nameer1811 <nameer.fahad@gmail.com>
2021-07-07 18:17:08 -0500

    Testing arguments

--- Tests/test_bifrost.py
+++ Tests/test_bifrost.py
@@ -5,7 +5,7 @@ import LWA_bifrost
 def test_args():
     args = LWA_bifrost.args_maker()  # This will give you the defaults
     args.offline = True  # manually set the offline argument
-    LWA_bifrost.main()
+    LWA_bifrost.main(args)
 
 	
 
