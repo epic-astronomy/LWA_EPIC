6b71833606280cdad0c1fbd39f19139f4f6580db
nameer1811 <nameer.fahad@gmail.com>
2021-07-07 19:24:41 -0500

    Creating Package

--- Tests/test_bifrost.py
+++ Tests/test_bifrost.py
@@ -6,6 +6,13 @@ from LWA import LWA_bifrost
 def test_args():
     args = LWA_bifrost.args_maker()  # This will give you the defaults
     args.offline = True  # manually set the offline argument
+    args.tbfile = "\data5\LWA_SV_data\data_raw\TBN\Jupiter\058161_000086727"
+    args.imagesize = 64
+    args.imageres = 1.79057
+    args.nts = 512
+    args.channels = 4
+    args.accumulate = 50
+    args.ints_per_file = 40
     LWA_bifrost.main(args)
 
 	
