4c3ef99ddadfa62be55efcf3fd177f5f95e7774f
nameer1811 <nameer.fahad@gmail.com>
2021-07-10 15:35:24 -0500

    Checking tests

--- Tests/test_bifrost.py
+++ Tests/test_bifrost.py
@@ -6,15 +6,6 @@ import os, os.path
 
 def test_args():
     args = LWA_bifrost.args_maker()  # This will give you the defaults
-    # args = {
-    #     "offline": True,
-    #     "tbnfile": "/data5/LWA_SV_data/data_raw/TBN/Jupiter/058161_000086727",
-    #     "imagesize": 64,
-    #     "imageres": 1.79057,
-    #     "nts": 512,
-    #     "channels": 4,
-    #     "accumulate": 50,
-    #     "ints_per_file": 40}
     args.offline = True  # manually set the offline argument
     args.tbnfile = "/data5/LWA_SV_data/data_raw/TBN/Jupiter/058161_000086727"
     args.imagesize = 64
