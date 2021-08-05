96a2f1e3e0eb4d7ee5f0420b168941365c2f5887
Adam Beardsley <adam.p.beardsley@gmail.com>
2021-08-05 09:25:12 -0500

    Adding minimal setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='LWA_EPIC',
+      description='Package to run EPIC on LWA stations',
+      url='http://github.com/epic-astronomy/LWA_EPIC',
+      author='EPIC Collaboration',
+      packages=['LWA_EPIC'],
+      zip_safe=False)
