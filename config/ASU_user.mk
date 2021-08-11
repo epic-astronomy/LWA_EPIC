d06922e114e2168e0ab5d82e4a3220657764e5c2
Adam Beardsley <adam.p.beardsley@gmail.com>
2021-08-11 16:06:39 -0500

    update asu make file for cuda 11.4

--- config/ASU_user.mk
+++ config/ASU_user.mk
@@ -20,7 +20,7 @@ ALIGNMENT ?= 4096 # Memory allocation alignment
 GPU_SHAREDMEM ?= 16384 # Fix GPU shared memory per device
 
 # check the NVCC version installed and its install directory
-CUDA_HOME     ?= /usr/local/cuda-10.2
+CUDA_HOME     ?= /usr/local/cuda-11.4
 
 CUDA_LIBDIR   ?= $(CUDA_HOME)/lib
 CUDA_LIBDIR64 ?= $(CUDA_HOME)/lib64
