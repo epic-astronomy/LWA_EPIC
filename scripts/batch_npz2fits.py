2db33284be8b2ed9c767a150dc7b00360b3c4086
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2021-08-16 09:52:13 -0700

    refactor npz2fits script write out the file only once, some precompute

--- scripts/batch_npz2fits.py
+++ scripts/batch_npz2fits.py
@@ -24,14 +24,14 @@ def epic2fits(filename, data, hdr, image_nums):
         List of image numbers from start of sequence. Length Ntimes.
 
     """
-    # Set up primary header and create file
     hdu = fits.PrimaryHDU()
     hdu.header["TELESCOP"] = hdr["telescope"]
     t0 = Time(
-        hdr["time_tag"] / hdr["FS"] + 1e-3 * hdr["accumulation_time"] / 2.,
+        hdr["time_tag"] / hdr["FS"] + 1e-3 * hdr["accumulation_time"] / 2.0,
         format="unix",
         precision=6,
-        location=(hdr["longitude"], hdr["latitude"]))
+        location=(hdr["longitude"], hdr["latitude"]),
+    )
     hdu.header["DATE-OBS"] = t0.isot
     hdu.header["BUNIT"] = hdr["data_units"]
     hdu.header["BSCALE"] = 1e0
@@ -46,9 +46,7 @@ def epic2fits(filename, data, hdr, image_nums):
     hdu.header["INTTIMU"] = "SECONDS"
     hdu.header["CFREQ"] = hdr["cfreq"]
     hdu.header["CFREQU"] = "HZ"
-
-    hdul = fits.HDUList([hdu])
-    hdul.writeto(filename, overwrite=True)
+    hdul = fits.HDUList(hdu)
 
     # Restructure data in preparation to stuff into fits
     data = data.transpose(0, 2, 1, 3, 4)  # Now (Ntimes, Npol, Nfreq, y, x)
@@ -58,45 +56,63 @@ def epic2fits(filename, data, hdr, image_nums):
     pol_order = np.argsort(pol_nums)[::-1]
     data = data[:, pol_order, :, :, :]
     # Break up real/imaginary
-    data = data[:, np.newaxis, :, :, :, :]  # Now (Ntimes, 2 (complex), Npol, Nfreq, y, x)
+    data = data[
+        :, np.newaxis, :, :, :, :
+    ]  # Now (Ntimes, 2 (complex), Npol, Nfreq, y, x)
     data = np.concatenate([data.real, data.imag], axis=1)
 
     if not isinstance(image_nums, (list, tuple, np.ndarray)):
         image_nums = [image_nums]
+    image_nums = np.asarray(image_nums)
 
     dt = TimeDelta(1e-3 * hdr["accumulation_time"], format="sec")
 
+    # some pre-compute
+    time_array = t0 + image_nums * dt
+    lsts = time_array.sidereal_time("apparent")
+    coords = SkyCoord(
+        lsts.deg, hdr["latitude"], obstime=time_array, unit="deg"
+    ).transform_to(FK5(equinox="J2000"))
+    dtheta_x = 2 * np.arcsin(0.5 / (hdr["grid_size_x"] * hdr["sampling_length_x"]))
+    dtheta_y = 2 * np.arcsin(0.5 / (hdr["grid_size_y"] * hdr["sampling_length_y"]))
+
+    crit_pix_x = float(hdr["grid_size_x"] / 2 + 1)
+    # Need to correct for shift in center pixel when we flipped dec dimension
+    # when writing npz, Only applies for even dimension size
+    crit_pix_y = float(hdr["grid_size_y"] / 2 + 1) - (hdr["grid_size_x"] + 1) % 2
+
+    delta_x = -dtheta_x * 180.0 / np.pi
+    delta_y = dtheta_y * 180.0 / np.pi
+    delta_f = hdr["bw"] / hdr["nchan"]
+    crit_pix_f = (hdr["nchan"] - 1) * 0.5 + 1  # +1 for FITS numbering
+
     for im_num, d in zip(image_nums, data):
-        hdu = fits.PrimaryHDU()
+        hdu = fits.ImageHDU(data=d)
         # Time
-        t = t0 + im_num * dt
-        lst = t.sidereal_time("apparent")
+        t = time_array[im_num]
+        lst = lsts[im_num]
         hdu.header["DATETIME"] = t.isot
         hdu.header["LST"] = lst.hour
         # Coordinates - sky
-        ra0 = lst.deg
-        dec0 = hdr["latitude"]
-        coord = SkyCoord(ra0, dec0, frame="fk5", equinox="J" + str(t.jyear), unit="deg")
-        coord.transform_to(FK5(equinox="J2000"))
+
         hdu.header["EQUINOX"] = "J2000"
+
         hdu.header["CTYPE1"] = "RA---SIN"
-        hdu.header["CRPIX1"] = float(hdr["grid_size_x"] / 2 + 1)
-        dtheta = 2 * np.arcsin(.5 / (hdr["grid_size_x"] * hdr["sampling_length_x"]))
-        hdu.header["CDELT1"] = -dtheta * 180. / np.pi
-        hdu.header["CRVAL1"] = coord.ra.deg
+        hdu.header["CRPIX1"] = crit_pix_x
+        hdu.header["CDELT1"] = delta_x
+        hdu.header["CRVAL1"] = coords[im_num].ra.deg
         hdu.header["CUNIT1"] = "deg"
         hdu.header["CTYPE2"] = "DEC--SIN"
-        # Need to correct for shift in center pixel when we flipped dec dimension when writing npz
-        # Only applies for even dimension size
-        hdu.header["CRPIX2"] = float(hdr["grid_size_y"] / 2 + 1) - (hdr["grid_size_x"] + 1) % 2
-        dtheta = 2 * np.arcsin(.5 / (hdr["grid_size_y"] * hdr["sampling_length_y"]))
-        hdu.header["CDELT2"] = dtheta * 180. / np.pi
-        hdu.header["CRVAL2"] = coord.dec.deg
+
+        hdu.header["CRPIX2"] = crit_pix_y
+
+        hdu.header["CDELT2"] = delta_y
+        hdu.header["CRVAL2"] = coords[im_num].dec.deg
         hdu.header["CUNIT2"] = "deg"
         # Coordinates - Freq
         hdu.header["CTYPE3"] = "FREQ"
-        hdu.header["CRPIX3"] = (hdr["nchan"] - 1) * 0.5 + 1  # +1 for FITS numbering
-        hdu.header["CDELT3"] = hdr["bw"] / hdr["nchan"]
+        hdu.header["CRPIX3"] = crit_pix_f
+        hdu.header["CDELT3"] = delta_f
         hdu.header["CRVAL3"] = hdr["cfreq"]
         hdu.header["CUNIT3"] = "Hz"
         # Coordinates - Stokes parameters
@@ -110,7 +126,9 @@ def epic2fits(filename, data, hdr, image_nums):
         hdu.header["CRPIX5"] = 1.0
         hdu.header["CDELT5"] = 1.0
 
-        fits.append(filename, d, hdu.header)
+        hdul.append(hdu)
+
+    hdul.writeto(filename, overwrite=True)
 
 
 a = argparse.ArgumentParser(description="Convert batch of npz files to fits")
