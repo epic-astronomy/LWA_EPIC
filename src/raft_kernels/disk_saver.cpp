046a709ba2de9cdb43d493947ad39a9f04004e6e
infinitron <karthikreddy311@gmail.com>
2023-02-28 18:39:17 -0700

    Test disk saver raft kernel

---
+++ src/raft_kernels/disk_saver.cpp
@@ -0,0 +1,37 @@
+#include "../ex/constants.h"
+#include "../ex/types.hpp"
+#include <chrono>
+#include <glog/logging.h>
+#include "../ex/py_funcs.hpp"
+#include <memory>
+#include <raft>
+#include <cstring>
+#include <raftio>
+
+template<class Payload>
+class DiskSaver_rft: public raft::kernel{
+    protected:
+    double m_ADP_time_from_unix_epoch_s{0};
+    public:
+    DiskSaver_rft():raft::kernel(){
+        input.addPort<Payload>("image");
+        m_ADP_time_from_unix_epoch_s = get_ADP_time_from_unix_epoch();
+    }
+
+    virtual raft::kstatus run() override{
+        Payload pld;
+        input["gulp"].pop(pld);
+
+        if(!pld){
+            LOG(WARNING)<<"Empty image received."
+            return raft::proceed;
+        }
+
+        auto& img_metadata = pld.get_mbuf()->get_metadataref();
+        auto imsize = std::any_cast<int>(img_metadata["grid_size"]);
+        auto nchan = std::any_cast<int>(img_metadata["nchan"]);
+        save_image(imsize, nchan, pld.get_mbuf()->get_data_ptr(), "test_image.png"s);
+
+        return raft::proceed;
+    }
+};
