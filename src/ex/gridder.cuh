2550e0755c7af3d29965eaf427c8360dae953f8c
infinitron <karthikreddy311@gmail.com>
2023-04-17 13:35:04 -0700

    On-chip gridder v1

--- src/ex/gridder.cuh
+++ src/ex/gridder.cuh
@@ -32,7 +32,7 @@ using namespace cufftdx;
  * @param smem Pointer to shared memory
  * @param gcf_tex GCF texture object
  * @return void
- * 
+ *
  * @relatesalso MOFFCuHandler
  */
 template<typename FFT, unsigned int Support, unsigned int NStands, std::enable_if_t<std::is_same<__half2, typename FFT::output_type::value_type>::value, bool> = true>
@@ -50,7 +50,7 @@ grid_dual_pol_dx5(cg::thread_block tb,
     constexpr float inv_support = 1.f / float(Support);
     constexpr float inv_half_support = 1.f / float(half_support);
     auto tile = cg::experimental::tiled_partition<Support * Support>(tb);
-   
+
     for (int ant = tile.meta_group_rank(); ant < NStands; ant += tile.meta_group_size()) {
         float scale = 1;
         thread_data[0] = __half2half2(0);
@@ -81,6 +81,79 @@ grid_dual_pol_dx5(cg::thread_block tb,
             atomicAdd(&smem[int(antx + u) + int(anty + v) * size_of<FFT>::value].x, thread_data[0].x);
             atomicAdd(&smem[int(antx + u) + int(anty + v) * size_of<FFT>::value].y, thread_data[0].y);
         }
+    }
+}
 
+/**
+ * @brief
+ *
+ * @tparam FFT FFT object constructed using cuFFTDx
+ * @tparam Support Support Size
+ * @tparam NStands Number of antennas
+ * @param thread_data Thread register array for use as temporary workspace
+ * @param f_eng Pointer to the F-Engine data to be gridded
+ * @param antpos Pointer to antenna position array
+ * @param phases Pointer to the phases array
+ * @param gcf_tex
+ * @return __device__
+ */
+template<typename FFT, unsigned int Support, unsigned int NStands, std::enable_if_t<std::is_same<__half2, typename FFT::output_type::value_type>::value, bool> = true>
+__device__ inline void
+grid_dual_pol_dx6(typename FFT::value_type thread_data[FFT::elements_per_thread],
+                  const cnib2* f_eng,
+                  const float3* __restrict__ antpos,
+                  const float4* __restrict__ phases,
+                  cudaTextureObject_t gcf_tex)
+{
+    constexpr float half_support = Support / 2.f;
+    constexpr float inv_half_support = 2. / float(Support);
+    constexpr int stride = float(size_of<FFT>::value) / FFT::elements_per_thread;
+    constexpr double inv_stride = 1. / double(stride);
+    static_assert(Support < size_of<FFT>::value / FFT::elements_per_thread, "Support size must be less than the stride");
+
+    for (int ant = 0; ant < NStands; ++ant) {
+        float antx = antpos[ant].x;
+        float anty = antpos[ant].y;
+
+        if (abs(anty - (threadIdx.y + 0.5)) >= half_support) {
+            continue;
+        }
+
+        // Determine if any pixel falls within the antenna's kernel in this row
+        // recall: pixels are arranged with a stride between them
+        // for example, in thread 0, the pixel indices in the row are
+        //  0, 8, 16, ... for a stride of 8.
+        // So if the support is less than the stride, only one pixel
+        // can fall within its kernel.
+
+        // as long as the support is smaller than the stride, rint or roundf should
+        // produce identical results
+
+        int thread_pix_idx = rintf((antx - threadIdx.x) * inv_stride);
+        thread_pix_idx = thread_pix_idx >= 0 ? thread_pix_idx : 0;
+        float ant_distx = abs(antx - (thread_pix_idx * stride + threadIdx.x + 0.5));
+
+        if (ant_distx >= half_support) {
+            continue;
+        }
+
+        auto phase_ant = phases[ant];
+        __half scale = tex2D<float>(
+          gcf_tex,
+          abs(ant_distx * inv_half_support),
+          abs((anty - (threadIdx.y + 0.5)) * inv_half_support));
+
+        typename FFT::value_type temp;
+
+        __cms_f<half2>(temp.x, float2{ phase_ant.x, phase_ant.y }, f_eng[ant].X, scale);
+        __cms_f<half2>(temp.y, float2{ phase_ant.z, phase_ant.w }, f_eng[ant].Y, scale);
+
+        // re-arrange the elements in  RRII layout
+        __half im = temp.x.y;
+        temp.x.y = temp.y.x;
+        temp.y.x = im;
+
+        thread_data[thread_pix_idx].x += temp.x;
+        thread_data[thread_pix_idx].y += temp.y;
     }
 }
