cbbf4598255429149ef9be57f186547c5e3df712
Karthik Reddy <karthikreddy311@gmail.com>
2023-08-13 23:19:25 -0700

    enumerate gpus

--- src/ex/host_helpers.cu
+++ src/ex/host_helpers.cu
@@ -1,22 +1,36 @@
 #include "host_helpers.h"
 #include <cstdint>
 
-
 template<typename T>
-int cu_mlock(T* ptr, size_t nbytes){
+int
+cu_mlock(T* ptr, size_t nbytes)
+{
     cuda_check_err(cudaHostRegister(ptr, nbytes, cudaHostRegisterDefault));
     return 0;
 }
 
 template<typename T>
-int cu_munlock(T* ptr){
+int
+cu_munlock(T* ptr)
+{
     cuda_check_err(cudaHostUnregister(ptr));
     return 0;
 }
 
+int
+get_ngpus()
+{
+    int n_devices;
+    cudaGetDeviceCount(&n_devices);
 
+    return n_devices;
+}
 
-template int cu_mlock<uint8_t>(uint8_t*, size_t);
-template int cu_munlock<uint8_t>(uint8_t*);
-template int cu_mlock<float>(float*, size_t);
-template int cu_munlock<float>(float*);
+template int
+cu_mlock<uint8_t>(uint8_t*, size_t);
+template int
+cu_munlock<uint8_t>(uint8_t*);
+template int
+cu_mlock<float>(float*, size_t);
+template int
+cu_munlock<float>(float*);
