c08b3dc4c529d8f5280134763daf527dcefdb030
Karthik Reddy <karthikreddy311@gmail.com>
2023-08-19 13:09:18 -0700

    Add kernel size to the orm type definition

--- src/ex/orm_types.hpp
+++ src/ex/orm_types.hpp
@@ -7,6 +7,8 @@
 #include <string>
 #include <vector>
 #include "types.hpp"
+#include <cstdlib>
+#include <cmath>
 
 struct EpicPixelTableMetaRows
 {
@@ -15,6 +17,7 @@ struct EpicPixelTableMetaRows
     std::string m_uuid;
     std::vector<int> source_ids;
     size_t nsrcs;
+    size_t kernel_size;
     std::vector<std::pair<int, int>> pixel_coords;
     std::vector<std::pair<int, int>> pixel_coords_sft;
     std::vector<std::pair<float, float>> pixel_lm;
@@ -27,6 +30,11 @@ struct EpicPixelTableMetaRows
     EpicPixelTableMetaRows(int ncoords, int n_sources)
     {
         nsrcs = n_sources;
+        m_ncoords = ncoords;
+
+        if(nsrcs==0 || m_ncoords==0){
+            return;
+        }
         source_ids.reserve(n_sources);
         pixel_coords.reserve(ncoords);
         pixel_coords_sft.reserve(ncoords);
@@ -34,7 +42,10 @@ struct EpicPixelTableMetaRows
         // pixel_skypos.reserve(ncoords);
         pixel_offst.reserve(ncoords);
 
-        m_ncoords = ncoords;
+
+        assert(is_kernel_size_valid(n_sources, ncoords) && "Invalid kernel size. Each source must have ksize * ksize number of sources.");
+
+        kernel_size = ncoords/n_sources;
     }
 
     EpicPixelTableMetaRows(){}
@@ -64,6 +75,12 @@ struct EpicPixelTableMetaRows
             //    (pixel_skypos != rhs.pixel_skypos) ||
                (pixel_offst != rhs.pixel_offst);
     }
+
+    bool is_kernel_size_valid(int nsrcs, int ncoords){
+        size_t nkernel_elems = ncoords/nsrcs;
+        size_t ksize = sqrt(nkernel_elems);
+        return nkernel_elems == ksize * ksize;
+    }
 };
 
 template<typename _Dtype>
@@ -75,9 +92,13 @@ struct EpicPixelTableDataRows : EpicPixelTableMetaRows
         int nsrcs;
         int nchan;
         bool check_opts()
-        {
-            return ncoords < 0 ? false : true;
-            return nchan < 0 ? false : true;
+        {   
+            bool flag = true;
+            flag &= ncoords < 0 ? false : true;
+            flag &= nchan < 0 ? false : true;
+            flag &= nsrcs <0 ? false : true;
+
+            return true;
         }
     };
 
@@ -114,22 +135,25 @@ struct EpicPixelTableDataRows : EpicPixelTableMetaRows
         m_ncoords = meta.m_ncoords;
         nsrcs = meta.nsrcs;
         source_ids = meta.source_ids;
+        kernel_size = meta.kernel_size;
     }
 
     void m_reset_buf() {}
 };
 
 EpicPixelTableMetaRows
-create_dummy_meta(int xdim, int ydim, int nsrcs=1, int kernel_size=1)
+create_dummy_meta(int xdim, int ydim, int nsrcs=1, int kernel_size=5)
 {   
     int ncoords = kernel_size * kernel_size * nsrcs;
     EpicPixelTableMetaRows meta(ncoords,nsrcs);
+
+    srand(time(NULL));
     
     meta.pixel_coords.insert(meta.pixel_coords.end(), ncoords, std::pair<int, int>(32, 33));
     meta.pixel_lm.insert(meta.pixel_lm.end(), ncoords, std::pair<int,int>(1,1));
     meta.pixel_offst.insert(meta.pixel_offst.end(), ncoords, std::pair<int,int>(0,0));
     meta.source_ids.insert(meta.source_ids.end(), ncoords, 1);
-    meta.meta_version = 42;
+    meta.meta_version = rand();
     meta.transform_pix_coords(xdim, ydim);
 
     return meta;
