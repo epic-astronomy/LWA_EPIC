10e462b55dffc93af1fd6f54aece495f2f740dc2
Karthik Reddy <karthikreddy311@gmail.com>
2023-08-19 08:23:37 -0700

    More overclocking commands

--- src/commands/nvidia-reset.bash
+++ src/commands/nvidia-reset.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ "$(id -u)" != "0" ]]
-  then echo "Insufficient permisssions. Are you root?"
-  exit
+if [[ "$(id -u)" != "0" ]]; then
+  echo "Insufficient permisssions. Are you root?"
+  exit -1
 fi
 
 #if the fan control commands fail as "Error assigning value XX to attribute 'GPUTargetFanSpeed'", check if the file /etc/X11/Xwrapper.config has the following
@@ -14,12 +14,12 @@ fi
 # If not create it and restart the X-server before running this script
 # see https://www.systutorials.com/docs/linux/man/5-Xwrapper.config/
 # for a description of these lines
-if [ ! -f "/etc/X11/Xwrapper.config" ];then
-    echo "The file /etc/X11/Xwrapper.config does not exist."
-    echo "Create this file and add the following lines before re-running this script"
-    echo "allowed_users=anybody"
-    echo "needs_root_rights=yes"
-    exit 1
+if [ ! -f "/etc/X11/Xwrapper.config" ]; then
+  echo "The file /etc/X11/Xwrapper.config does not exist."
+  echo "Create this file and add the following lines before re-running this script"
+  echo "allowed_users=anybody"
+  echo "needs_root_rights=yes"
+  exit 1
 fi
 
 uid="$(id -u gdm)"
@@ -30,16 +30,22 @@ NUM_FANS=$(DISPLAY=:0 XAUTHORITY=/run/user/${uid}/gdm/Xauthority nvidia-settings
 
 echo "Found ${NUM_GPUS} GPUs and ${NUM_FANS} Fans"
 
-# For each GPU, enable fan control.
-for i in $(seq 0 1 $(echo "$NUM_GPUS-1" | bc -l));
-do
-    DISPLAY=:0 XAUTHORITY=/run/user/${uid}/gdm/Xauthority nvidia-settings --verbose=all -a "[gpu:$i]/GPUFanControlState=0"
+# For each GPU, disable fan control, reset power limit and power mizer mode.
+for i in $(seq 0 1 $(echo "$NUM_GPUS-1" | bc -l)); do
+  DISPLAY=:0 XAUTHORITY=/run/user/${uid}/gdm/Xauthority nvidia-settings --verbose=all -a "[gpu:$i]/GPUFanControlState=0"
+
+  DISPLAY=:0 XAUTHORITY=/run/user/${uid}/gdm/Xauthority nvidia-settings --verbose=all -a "[gpu:$i]/GPUPowerMizerMode=0"
+
+  DISPLAY=:0 XAUTHORITY=/run/user/${uid}/gdm/Xauthority nvidia-smi -i $i -pl 450
+
 done
 
-# Clock the GPUs to maximum
+# Clock the GPU clocks
 nvidia-smi -pm 0
 nvidia-smi -rgc
 nvidia-smi -rmc
 
+echo "Waiting for GPU spin down..."
+sleep 4
 # Print the status
 nvidia-smi --query-gpu=index,gpu_name,persistence_mode,clocks.sm,clocks.mem,temperature.gpu,fan.speed,power.draw,pstate --format=csv
