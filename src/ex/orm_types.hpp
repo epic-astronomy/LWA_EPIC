483a92c219eb373aad2f8ef84723906525e6ae15
Karthik Reddy <karthikreddy311@gmail.com>
2023-10-12 17:45:53 -0600

    Fix incorrect vector allocation

--- src/ex/orm_types.hpp
+++ src/ex/orm_types.hpp
@@ -23,9 +23,11 @@
 #ifndef SRC_EX_ORM_TYPES_HPP_
 #define SRC_EX_ORM_TYPES_HPP_
 
+#include <glog/logging.h>
 #include <hwy/aligned_allocator.h>
 
 #include <algorithm>
+#include <cassert>
 #include <cmath>
 #include <cstdlib>
 #include <string>
@@ -39,7 +41,8 @@ struct EpicPixelTableMetaRows {
   std::string m_uuid;
   std::vector<int> source_ids;
   size_t nsrcs;
-  size_t kernel_size;
+  size_t m_kernel_size;
+  size_t m_kernel_dim;
   std::vector<std::pair<int, int>> pixel_coords;
   std::vector<std::pair<int, int>> pixel_coords_sft;
   std::vector<std::pair<float, float>> pixel_lm;
@@ -49,29 +52,45 @@ struct EpicPixelTableMetaRows {
   size_t m_ncoords;
   int meta_version{-1};
 
-  EpicPixelTableMetaRows(int ncoords, int n_sources) {
+  EpicPixelTableMetaRows(int ncoords, int n_sources, int kernel_dim = 5) {
     nsrcs = n_sources;
     m_ncoords = ncoords;
+    m_kernel_dim = kernel_dim;
 
     if (nsrcs == 0 || m_ncoords == 0) {
       return;
     }
-    source_ids.reserve(n_sources);
-    pixel_coords.reserve(ncoords);
-    pixel_coords_sft.reserve(ncoords);
-    pixel_lm.reserve(ncoords);
-    // pixel_skypos.reserve(ncoords);
-    pixel_offst.reserve(ncoords);
+    source_ids.resize(n_sources);
+    pixel_coords.resize(ncoords);
+    pixel_coords_sft.resize(ncoords);
+    pixel_lm.resize(ncoords);
+    // pixel_skypos.resize(ncoords);
+    pixel_offst.resize(ncoords);
 
     assert(IsKernelSizeValid(n_sources, ncoords) &&
            "Invalid kernel size. Each source must have ksize * ksize number of "
            "sources.");
 
-    kernel_size = ncoords / n_sources;
+    m_kernel_size = ncoords / nsrcs;
+
+    VLOG(3) << "Meta rows";
+    VLOG(3) << "Nsrc: " << nsrcs << " Ncoords: " << m_ncoords
+            << " Kernel size: " << m_kernel_size
+            << " Kernel dim: " << m_kernel_dim;
   }
 
   EpicPixelTableMetaRows() {}
 
+  /**
+   * @brief Transform pixel indices into array indices
+   *
+   * The pixel indexing starts from the bottom left corner of the image, while
+   * the array indexing starts from the top left corner. This function
+   * performs the appropriate transformation between pixel and array indices.
+   *
+   * @param xdim X-dimension of the image
+   * @param ydim Y-dimension of the image
+   */
   void TransformPixCoords(int xdim, int ydim) {
     for (size_t i = 0; i < m_ncoords; ++i) {
       int x = pixel_coords[i].first;
@@ -84,10 +103,18 @@ struct EpicPixelTableMetaRows {
       // circshift
       x = (x + xdim / 2) % xdim;
       y = (y + ydim / 2) % ydim;
-      pixel_coords_sft.push_back(std::pair<int, int>(x, y));
+      pixel_coords_sft[i] = std::pair<int, int>(x, y);
     }
   }
 
+  /**
+   * @brief Compare this set of coords with another.
+   *
+   * Use this to check for any updates
+   *
+   * @param rhs Coordinate set to compare to
+   * @return Whether or not the coordinate sets are identical
+   */
   bool diff(const EpicPixelTableMetaRows& rhs) {
     return (m_ncoords != rhs.m_ncoords) || (pixel_coords != rhs.pixel_coords) ||
            (pixel_lm != rhs.pixel_lm) ||
@@ -95,10 +122,30 @@ struct EpicPixelTableMetaRows {
            (pixel_offst != rhs.pixel_offst);
   }
 
+  /**
+   * @brief Check if a square shaped kernel produces the current coordinate
+   * set
+   *
+   * @param nsrcs Total number of sources
+   * @param ncoords Total number of coordinates
+   * @return Returns whether the kernel is square-shaped
+   */
   bool IsKernelSizeValid(int nsrcs, int ncoords) {
     size_t nkernel_elems = ncoords / nsrcs;
     size_t ksize = sqrt(nkernel_elems);
     return nkernel_elems == ksize * ksize;
+  }
+
+  void print() {
+    VLOG(3) << "Printing indices";
+    VLOG(3) << pixel_coords.size() << " " << pixel_coords_sft.size() << " "
+            << pixel_lm.size() << " " << pixel_offst.size();
+    for (int i = 0; i < m_ncoords; ++i) {
+      VLOG(3) << pixel_coords[i].first << " " << pixel_coords[i].second << " "
+              << pixel_coords_sft[i].first << " " << pixel_coords_sft[i].second
+              << " " << pixel_lm[i].first << " " << pixel_lm[i].second << " "
+              << pixel_offst[i].first << " " << pixel_offst[i].second;
+    }
   }
 };
 
@@ -108,13 +155,19 @@ struct EpicPixelTableDataRows : EpicPixelTableMetaRows {
     int ncoords;
     int nsrcs;
     int nchan;
+    int kernel_dim;
     bool CheckOpts() {
       bool flag = true;
       flag &= ncoords < 0 ? false : true;
       flag &= nchan < 0 ? false : true;
       flag &= nsrcs < 0 ? false : true;
+      flag &= kernel_dim < 0 ? false : true;
 
-      return true;
+      if (!flag) {
+        VLOG(3) << ncoords << " " << nsrcs << " " << nchan << " " << kernel_dim;
+      }
+
+      return flag;
     }
   };
 
@@ -124,9 +177,12 @@ struct EpicPixelTableDataRows : EpicPixelTableMetaRows {
 
   hwy::AlignedFreeUniquePtr<_Dtype[]> pixel_values;
   explicit EpicPixelTableDataRows(config_t config)
-      : EpicPixelTableMetaRows(config.ncoords, config.nsrcs) {
-    pixel_values = std::move(
-        hwy::AllocateAligned<_Dtype>(config.ncoords * NSTOKES * config.nchan));
+      : EpicPixelTableMetaRows(config.ncoords, config.nsrcs,
+                               config.kernel_dim) {
+    if (config.ncoords > 0) {
+      pixel_values = std::move(hwy::AllocateAligned<_Dtype>(
+          config.ncoords * NSTOKES * config.nchan));
+    }
     m_nchan = config.nchan;
   }
 
@@ -134,6 +190,7 @@ struct EpicPixelTableDataRows : EpicPixelTableMetaRows {
     if (meta_version == meta.meta_version) {
       return;
     }
+    VLOG(3) << "Copying metadata";
 
     if (m_ncoords != meta.m_ncoords) {
       pixel_values.reset();
@@ -149,16 +206,17 @@ struct EpicPixelTableDataRows : EpicPixelTableMetaRows {
     m_ncoords = meta.m_ncoords;
     nsrcs = meta.nsrcs;
     source_ids = meta.source_ids;
-    kernel_size = meta.kernel_size;
+    m_kernel_size = meta.m_kernel_size;
+    m_kernel_dim = meta.m_kernel_dim;
   }
 
   void ResetBuf() {}
 };
 
-EpicPixelTableMetaRows create_dummy_meta(int xdim, int ydim, int nsrcs = 1,
-                                         int kernel_size = 5) {
-  int ncoords = kernel_size * kernel_size * nsrcs;
-  EpicPixelTableMetaRows meta(ncoords, nsrcs);
+EpicPixelTableMetaRows CreateDummyMeta(int xdim, int ydim, int nsrcs = 1,
+                                       int kernel_dim = 5) {
+  int ncoords = kernel_dim * kernel_dim * nsrcs;
+  EpicPixelTableMetaRows meta(ncoords, nsrcs, kernel_dim);
 
   srand(time(NULL));
 
