92bc0933180f8fecd59f74d23d52ffaedbac7be1
Karthik Reddy <karthikreddy311@gmail.com>
2023-11-11 19:31:23 -0700

    unlock buffer inside kernel during health checks

--- src/raft_kernels/chan_reducer.hpp
+++ src/raft_kernels/chan_reducer.hpp
@@ -116,6 +116,8 @@ class ChanReducerRft : public raft::kernel {
         std::get<int64_t>(in_pld.get_mbuf()->GetMetadataRef()["chan0"]);
     if (m_hc_chans.count(chan0) > 0) {
       LOG_EVERY_N(WARNING, 3000) << "Health Checks. Ignoring data";
+      in_pld = _PldIn();  // this releases the lock without waiting for raft's
+                          // runtime to destroy the object
       return raft::proceed;
     }
 
