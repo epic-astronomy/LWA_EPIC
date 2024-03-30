8ba8140fd56d6e224ef1a58ae4f936b2f06ab279
Karthik Reddy <karthikreddy311@gmail.com>
2024-03-30 05:10:15 +0000

    perf: increased the buufer count to accomodate db ingestion delays

--- src/raft_kernels/chan_reducer.hpp
+++ src/raft_kernels/chan_reducer.hpp
@@ -54,7 +54,7 @@ class ChanReducerRft : public raft::kernel {
   float m_norm{1};
   bool m_is_chan_avg{false};
   std::unique_ptr<BufferMngr> m_buf_mngr{nullptr};
-  static constexpr size_t m_nbufs{200};
+  static constexpr size_t m_nbufs{300};
   static constexpr size_t m_max_buf_reqs{10};
   size_t m_xdim{128};
   size_t m_ydim{128};
